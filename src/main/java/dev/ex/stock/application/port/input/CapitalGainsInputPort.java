251f17019e51c506160d66a325c1fc163c763cda
carolusquintus <carloschars@gmail.com>
2023-08-21 22:06:44 -0600

    Adding StockReader as dependency from InputPort

--- src/main/java/dev/ex/stock/application/port/input/CapitalGainsInputPort.java
+++ src/main/java/dev/ex/stock/application/port/input/CapitalGainsInputPort.java
@@ -1,11 +1,23 @@
 package dev.ex.stock.application.port.input;
 
 import dev.ex.stock.application.usecase.CapitalGainsUseCase;
+import dev.ex.stock.domain.service.StockReader;
+import dev.ex.stock.domain.service.shared.StockService;
+
+import java.io.InputStream;
 
 public class CapitalGainsInputPort implements CapitalGainsUseCase {
 
+    private StockService<InputStream, String> reader;
+
+    public CapitalGainsInputPort() {
+        this.reader = new StockReader();
+    }
+
     @Override
     public String calculateTaxes() {
+        var lines = reader.process(System.in);
+        System.out.println(lines);
         return null;
     }
 
