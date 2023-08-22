78e2c1b3bfb8386f91608c8441659869878dd20d
carolusquintus <carloschars@gmail.com>
2023-08-21 20:35:45 -0600

    Renaming to CapitalGainsApp to CapitalGainsCommand

--- src/main/java/dev/ex/stock/infra/input/cli/CapitalGainsCommand.java
+++ src/main/java/dev/ex/stock/infra/input/cli/CapitalGainsCommand.java
@@ -1,6 +1,6 @@
 package dev.ex.stock.infra.input.cli;
 
-public class CapitalGainsApp {
+public class CapitalGainsCommand {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
