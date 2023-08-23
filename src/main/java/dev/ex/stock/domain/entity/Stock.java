a387f8b43f263643761bef83c983302770e5cce5
carolusquintus <carloschars@gmail.com>
2023-08-22 19:42:26 -0600

    Set @JsonProperty("unit-cost") to deserialize

--- src/main/java/dev/ex/stock/domain/entity/Stock.java
+++ src/main/java/dev/ex/stock/domain/entity/Stock.java
@@ -1,9 +1,16 @@
 package dev.ex.stock.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public record Stock(
+
     Operation operation,
+
+    @JsonProperty("unit-cost")
     BigDecimal unitCost,
+
     Long quantity
+
 ) {}
