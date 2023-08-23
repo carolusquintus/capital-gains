6c08c6d11cc9b20640f937b94d63e4aec0f79252
carolusquintus <carloschars@gmail.com>
2023-08-22 20:57:37 -0600

    Adding dependency of StockParser in CapitalGainsInputPort

--- src/main/java/dev/ex/stock/application/port/input/CapitalGainsInputPort.java
+++ src/main/java/dev/ex/stock/application/port/input/CapitalGainsInputPort.java
@@ -1,23 +1,28 @@
 package dev.ex.stock.application.port.input;
 
 import dev.ex.stock.application.usecase.CapitalGainsUseCase;
+import dev.ex.stock.domain.entity.Stock;
+import dev.ex.stock.domain.service.StockParser;
 import dev.ex.stock.domain.service.StockReader;
 import dev.ex.stock.domain.service.shared.StockService;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class CapitalGainsInputPort implements CapitalGainsUseCase {
 
     private StockService<InputStream, String> reader;
+    private StockService<List<String>, List<Stock>> parser;
 
     public CapitalGainsInputPort() {
         this.reader = new StockReader();
+        this.parser = new StockParser();
     }
 
     @Override
     public String calculateTaxes() {
         var lines = reader.process(System.in);
-        System.out.println(lines);
+        var stocks = parser.process(lines);
         return null;
     }
 
