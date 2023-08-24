baeff6ebd70a791b9067882b23cadb57e46b86c9
carolusquintus <carloschars@gmail.com>
2023-08-23 21:54:25 -0600

    Validation against fixed TOTAL_AMOUNT

--- src/main/java/dev/ex/stock/domain/service/TaxCalculator.java
+++ src/main/java/dev/ex/stock/domain/service/TaxCalculator.java
@@ -5,12 +5,16 @@ import dev.ex.stock.domain.entity.Tax;
 import dev.ex.stock.domain.service.shared.Service;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
-import static dev.ex.stock.domain.entity.Operation.BUY;
-
 public class TaxCalculator implements Service<List<List<Stock>>, List<List<Tax>>> {
+
+    private static final BigInteger PERCENTAGE_TAX = new BigInteger("20");
+    private static final BigDecimal TOTAL_AMOUNT = new BigDecimal("20000.00");
+
+    private static final Tax ZERO_TAX = new Tax(new BigDecimal("0.00"));
 
     @Override
     public List<List<Tax>> process(List<List<Stock>> input) {
@@ -20,17 +24,27 @@ public class TaxCalculator implements Service<List<List<Stock>>, List<List<Tax>>
     }
 
     private List<Tax> calculateTaxes(List<Stock> stocks) {
-        final var zeroTax = new Tax(new BigDecimal("0.00"));
         var taxes = new ArrayList<Tax>();
 
         for (var stock: stocks) {
             switch (stock.operation()) {
-                case BUY -> taxes.add(zeroTax);
+                case BUY -> taxes.add(ZERO_TAX);
                 case SELL -> {
+                    if (isLowerEqualTotalAmount(stock)) {
+                        taxes.add(ZERO_TAX);
+                    }
                 }
             }
         }
 
         return taxes;
     }
+
+    private boolean isLowerEqualTotalAmount(Stock stock) {
+        var totalAmountCalculated = stock.unitCost().multiply(new BigDecimal(stock.quantity()));
+        var comparisonResult = totalAmountCalculated.compareTo(TOTAL_AMOUNT);
+        return comparisonResult <= 0;
+    }
+
+
 }
