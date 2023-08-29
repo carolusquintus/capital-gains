c1254147b39539cdcc83bd95d51c5331ceefcee6
carolusquintus <carloschars@gmail.com>
2023-08-29 10:21:39 -0600

    StockReaderSpec spock test specification

---
+++ src/test/groovy/dev/ex/stock/domain/service/StockReaderSpec.groovy
@@ -0,0 +1,77 @@
+package dev.ex.stock.domain.service
+
+import spock.lang.Specification
+
+class StockReaderSpec extends Specification {
+
+    def "Process input when String is single line with one array"() {
+        given:
+        def reader = new StockReader()
+
+        and:
+        def input = '[{"operation":"buy", "unit-cost":10.00, "quantity": 100},{"operation":"sell", "unit-cost":15.00, "quantity": 50},{"operation":"sell", "unit-cost":15.00, "quantity": 50}]'
+
+        and:
+        def stream = new ByteArrayInputStream(input.getBytes())
+
+        when:
+        def result = reader.process(stream)
+
+        then:
+        result
+        !result.empty
+        result.size() == 1
+    }
+
+    def "Process input when String is multi line with one array"() {
+        given:
+        def reader = new StockReader()
+
+        and:
+        def input = '''[
+            {"operation":"buy", "unit-cost":10.00, "quantity": 100},
+            {"operation":"sell", "unit-cost":15.00, "quantity": 50},
+            {"operation":"sell", "unit-cost":15.00, "quantity": 50}
+        ]'''
+
+        and:
+        def stream = new ByteArrayInputStream(input.getBytes())
+
+        when:
+        def result = reader.process(stream)
+
+        then:
+        result
+        !result.empty
+        result.size() == 1
+    }
+
+    def "Process input when String is multi line with two arrays"() {
+        given:
+        def reader = new StockReader()
+
+        and:
+        def input = '''[
+            {"operation":"buy", "unit-cost":10.00, "quantity": 100},
+            {"operation":"sell", "unit-cost":15.00, "quantity": 50},
+            {"operation":"sell", "unit-cost":15.00, "quantity": 50}
+        ]
+        [
+            {"operation":"buy", "unit-cost":10.00, "quantity": 10000},
+            {"operation":"sell", "unit-cost":20.00, "quantity": 5000},
+            {"operation":"sell", "unit-cost":5.00, "quantity": 5000}
+        ]'''
+
+        and:
+        def stream = new ByteArrayInputStream(input.getBytes())
+
+        when:
+        def result = reader.process(stream)
+
+        then:
+        result
+        !result.empty
+        result.size() == 2
+    }
+
+}
